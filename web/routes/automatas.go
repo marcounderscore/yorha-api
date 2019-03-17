bbf14cc06f05b5728d402cf582befb51cec6159a
marco <marcovazquez77@hotmail.com>
2019-03-17 15:41:43 -0600

    Route function added to update an auotomata

--- web/routes/automatas.go
+++ web/routes/automatas.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"errors"
 	"iris"
 	"yorha-api/datamodels"
 	"yorha-api/services"
@@ -23,7 +24,7 @@ func InsertAutomata(ctx iris.Context, service services.AutomataService) (datamod
 	if err := ctx.ReadJSON(&automata); err != nil {
 		ctx.StatusCode(iris.StatusBadRequest)
 		ctx.WriteString(err.Error())
-		return datamodels.Automata{}, err
+		return datamodels.Automata{}, errors.New("Json request is not correct at all")
 	}
 
 	name := automata.Name
@@ -32,4 +33,22 @@ func InsertAutomata(ctx iris.Context, service services.AutomataService) (datamod
 	photo := automata.Photo
 
 	return service.Create(name, occupation, race, photo)
+}
+
+// UpdateAutomata modify a current record in the database. http://localhost:8080/automatas/1
+func UpdateAutomata(ctx iris.Context, service services.AutomataService, id uint) (datamodels.Automata, bool, error) {
+	var automata datamodels.Automata
+
+	if err := ctx.ReadJSON(&automata); err != nil {
+		ctx.StatusCode(iris.StatusBadRequest)
+		ctx.WriteString(err.Error())
+		return datamodels.Automata{}, true, errors.New("Json request is not correct at all")
+	}
+
+	name := automata.Name
+	occupation := automata.Occupation
+	race := automata.Race
+	photo := automata.Photo
+
+	return service.Update(id, name, occupation, race, photo)
 }
