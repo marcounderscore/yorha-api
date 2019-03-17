af7a877bd7a59088ac9df3e70a31a947f996b3da
marco <marcovazquez77@hotmail.com>
2019-03-17 14:20:13 -0600

    New method in routes to insert an automata added

--- web/routes/automatas.go
+++ web/routes/automatas.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"iris"
 	"yorha-api/datamodels"
 	"yorha-api/services"
 )
@@ -13,4 +14,22 @@ func Automatas(service services.AutomataService) (results []datamodels.Automata)
 // AutomataByID return the current automata. http://localhost:8080/automatas/1
 func AutomataByID(service services.AutomataService, id uint) (automata datamodels.Automata, found bool) {
 	return service.GetByID(id)
+}
+
+// InsertAutomata create a new record in the database. http://localhost:8080/automatas
+func InsertAutomata(ctx iris.Context, service services.AutomataService) (datamodels.Automata, error) {
+	var automata datamodels.Automata
+
+	if err := ctx.ReadJSON(&automata); err != nil {
+		ctx.StatusCode(iris.StatusBadRequest)
+		ctx.WriteString(err.Error())
+		return datamodels.Automata{}, err
+	}
+
+	name := automata.Name
+	occupation := automata.Occupation
+	race := automata.Race
+	photo := automata.Photo
+
+	return service.Create(name, occupation, race, photo)
 }
