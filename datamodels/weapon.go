58e0c89e54f6ba745e57bc63f752b05039d370d1
marco <marcovazquez77@hotmail.com>
2019-03-19 01:03:36 -0600

    Model wepon modified to allow embeddeding classes

--- datamodels/weapon.go
+++ datamodels/weapon.go
@@ -6,8 +6,9 @@ import "github.com/jinzhu/gorm"
 type Weapon struct {
 	gorm.Model
 	Name        string `gorm:"size:100"`
-	Class       string `gorm:"size:100"`
 	Description string `gorm:"size:100"`
 	Ability     string `gorm:"size:100"`
 	Photo       string `gorm:"size:500"`
+	ClassID     uint
+	Class       Class
 }
