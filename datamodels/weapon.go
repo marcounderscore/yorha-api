d7e1b53157aa4aeaf4a05d52649a551ca872d4aa
marco <marcovazquez77@hotmail.com>
2019-03-23 18:36:29 -0600

    Model modified in order to allow new columns

--- datamodels/weapon.go
+++ datamodels/weapon.go
@@ -5,10 +5,11 @@ import "github.com/jinzhu/gorm"
 // Weapon is a *gorm.DB type
 type Weapon struct {
 	gorm.Model
-	Name        string `gorm:"size:100"`
-	Description string `gorm:"size:100"`
-	Ability     string `gorm:"size:100"`
-	Photo       string `gorm:"size:500"`
-	ClassID     uint
-	Class       Class
+	Name      string `gorm:"size:100"`
+	MinAttack int
+	MaxAttack int
+	Ability   string `gorm:"size:100"`
+	Photo     string `gorm:"size:500"`
+	ClassID   uint
+	Class     Class
 }
